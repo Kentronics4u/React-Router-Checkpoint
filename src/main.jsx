ddbc507179b9e8372b2963dc9e9aa370323cac53
IG <ifediorachukwu@yahoo.com>
2023-11-24 23:45:17 +0100

    bg update

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import "./index.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
-
 import "@fontsource/lakki-reddy";
 
 ReactDOM.createRoot(document.getElementById("root")).render(

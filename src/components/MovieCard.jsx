87489805195a73eaeff3631d07349120450bb085
IG <ifediorachukwu@yahoo.com>
2023-11-25 17:07:50 +0100

    card img height fixed

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,13 +9,11 @@ const MovieCard = ({ Title, Description, PosterURL, Rating }) => {
     <Card
       className="w-[65vw] sm:w-60"
       hoverable
-      cover={
-        <img alt="example" src={PosterURL} className=" sm:h-auto sm:max-h-72" />
-      }
+      cover={<img alt="example" src={PosterURL} className="  sm:h-72" />}
       bodyStyle={{ padding: 10, textAlign: "center" }}
     >
       <Meta
-        className=" sm:min-h-[120px]"
+        className=" sm:min-h-[120px] new"
         title={Title}
         description={Description.slice(0, 100)}
       />

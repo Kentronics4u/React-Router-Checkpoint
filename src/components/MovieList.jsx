55b15d89ba3a71957820b0311347657972984cfa
IG <ifediorachukwu@yahoo.com>
2023-11-25 11:36:10 +0100

    bg sm fixed

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 const MovieList = ({ myMovies }) => {
   return (
     // movieList Component to hold all movies being displayed
-    <div className="max-w-4xl mt-8 mx-auto sm:bg-white">
+    <div className="max-w-4xl mt-8 mx-auto ">
       <div className="grid place-items-center sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {myMovies.map((item) => (
           <MovieCard key={item.id} {...item} />

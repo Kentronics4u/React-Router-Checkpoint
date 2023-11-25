9d9a0858b03b0dfacf0a422120861b9490a51538
IG <ifediorachukwu@yahoo.com>
2023-11-25 17:56:53 +0100

    checking depl error

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,6 @@ import "./index.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/lakki-reddy";
-// Supports weights 400-700
-import "@fontsource-variable/caveat";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

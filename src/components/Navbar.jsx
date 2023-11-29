812cb2b296f58de06620542570e95bbd25cc300f
IG <ifediorachukwu@yahoo.com>
2023-11-29 15:46:52 +0100

    logo display on small

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,15 @@ const Navbar = () => {
   //Navbar component
   return (
     <nav className="shadow-orange-950  shadow-lg pl-16 flex items-center space-x-2 justify-center sticky top-0 z-10 bg-white bg-opacity-100">
-      <h1 className="site--hdg  text-red-950 -rotate-6 hover:drop-shadow-2xl tracking-wide">
-        FILM
+      <h1 className="site--hdg  text-red-950 -rotate-6 hover:drop-shadow-2xl tracking-wide ">
+        <span className="max-sm:hidden">FILM</span>
         <span>
-          <img src="/logo-svg.svg" className="h-12 rotate-180" />
+          <img
+            src="/logo-svg.svg"
+            className="h-12 rotate-180 max-sm:rotate-0"
+          />
         </span>
-        FREAKS
+        <span>FREAKS</span>
       </h1>
     </nav>
   );
